4032086113fb3fa4dcccb0d378370a66abfeb093
Xoma <xomyakes@gmail.com>
2021-04-01 08:56:45 +0300

    Fill Tables Alpha

--- fill_tables/scripts/fill_tables.py
+++ fill_tables/scripts/fill_tables.py
@@ -1,15 +1,34 @@
 import os
 import openpyxl
 import pandas as pd
+import gspread
 import requests
 import google.auth
 from google.oauth2 import service_account
 from gspread_pandas import Spread, Client 
+from gspread_formatting import *
 
 #Основная функция 
 def main():
-    print('Fill tables greets u!')
+    print('Starting to fill tables')
     fill_table()
+# Функция заполнения таблиц
+def fill_table():
+    try:
+        #table = Spread('PnL New')
+        table = Spread("https://docs.google.com/spreadsheets/d/1nXYmAIztT0Zm_wOZf9GYPR_fYHsBxGeXGc3buMv1flg")
+        print("Successfull authorized")
+    except requests.exceptions.RequestException:
+        print("Can't connect to Google API")
+        raise SystemExit()
+    reports = load_reports()
+    for report in reports:
+        print('Working with {}'.format(report.burse))
+        fill_month(table,report)
+        fill_Pertrader_month(table,report)
+        fill_balances(table,report)
+    check(table,report)
+    print("Table filled")
 # Функция загрузки отчётов из таблиц
 def load_reports():
     reports = []
@@ -17,56 +36,56 @@ def load_reports():
     reports_folder = os.listdir(os.getcwd() + '/reports')
     for report_file in reports_folder:
         if report_file[0] != '.':
-            workbook = openpyxl.load_workbook(filename = "./reports/" + report_file)
+            try:
+                workbook = openpyxl.load_workbook(filename = "./reports/" + report_file)
+            except requests.exceptions.RequestException:
+                print("Can't open report " + report_file)
+                raise SystemExit()
             reports.append(Report(str(report_file),workbook))
             reports_counter += 1
     print("Reports loaded")
     return reports
-# Функция заполнения таблиц
-def fill_table():
-    try:
-        table = Spread('PnL New')
-        print("Successfull authorized")
-    except requests.exceptions.RequestException:
-        print("Can't connect to Google API")
-        raise SystemExit()
-    reports = load_reports()
-    for report in reports:
-        fill_month(table,report)
-        fill_Pertrader_month(table,report)
-        fill_balances(table,report)
-    print("Table filled")
 # Заполнение листа месяца
 def fill_month(table,report):
-    sheet_month = table.worksheet(get_month_from_numbers(report.month) + ' ' + report.year)
+
+    #Определение диапазона копируемых ячеек
+    report_sheet = report.workbook.get_sheet_by_name('Total')
+    report_row = 2
+    first_report_col = 1
+    last_report_col = first_report_col + 3
+    copied_row = report_sheet[number_to_fucking_a1(first_report_col) + str(report_row):number_to_fucking_a1(last_report_col) + str(report_row)]
+    flat_copied_row = []
+    for tup in copied_row:
+        for t in tup:
+            flat_copied_row.append(t.value)
+
+    #Определение диапазона заполняемых ячеек
+    sheet_month = table.find_sheet(get_month_from_numbers(report.month) + ' ' + str(report.year))
     filling_row = sheet_month.find(report.get_date()).row
     if (report.session == 'B'):
-        filling_col = sheet_month.find(report.burse).col 
+        first_filling_col = sheet_month.find(report.burse).col 
     else:
-        filling_col = sheet_month.find(report.burse + ' Night').col 
-    col_counter = 1
-    cell_value = report.workbook.get_sheet_by_name("Total").cell(2,col_counter).value
-    while cell_value != None:
-        sheet_month.update_cell(filling_row, filling_col + col_counter - 1, cell_value)
-        col_counter += 1
-        cell_value = report.workbook.get_sheet_by_name("Total").cell(2,col_counter).value
-    print('Month filled')
+        first_filling_col = sheet_month.find(report.burse + ' Night').col 
+    last_filling_col = first_filling_col + last_report_col - first_report_col
+    
+    table.update_cells((filling_row,first_filling_col),(filling_row,last_filling_col),list(flat_copied_row),sheet_month)
+
+    print('Month of {} succesfull filled'.format(report.burse))
 #Заполнение листа трейдеров
 def fill_Pertrader_month(table,report):
 
     #Определение диапазона копируемых ячеек
-    print('Works on PerTrader sheet')
     report_sheet = report.workbook.get_sheet_by_name('Per_trader')
     first_report_row = 2
     first_report_col = 1
     last_report_row = report_sheet.max_row
     last_report_col = first_report_col + 4
-    copied_range_str = range_to_a1(first_report_row, first_report_col, last_report_row, last_report_col)
     copied_range = report_sheet[number_to_fucking_a1(first_report_col) + str(first_report_row):number_to_fucking_a1(last_report_col) + str(last_report_row)]
     flat_copied_range = []
     for tup in copied_range:
         for t in tup:
             flat_copied_range.append(t.value)
+    #print("Copying cells from range {}{}:{}{}".format(number_to_fucking_a1(first_report_col),first_report_row,number_to_fucking_a1(last_report_col),last_report_row))
 
     #Определение диапазона заполняемых ячеек
     sheet_PerTrader = table.find_sheet('PerTrader_' + str(get_month_from_numbers(report.month)[0:3]))
@@ -75,24 +94,28 @@ def fill_Pertrader_month(table,report):
     first_filling_row = len(session_column) + 1
     last_filling_col = first_filling_col + last_report_col - first_report_col
     last_filling_row = first_filling_row + last_report_row - first_report_row
-    filling_range = range_to_a1(first_filling_row, first_filling_col, last_filling_row, last_filling_col)
-    print('Copying {} from report_sheet to {} in PerTrader_sheet.'.format(copied_range_str,filling_range))
-    #print('Number of values = ' + str(len(flat_copied_range)))
-    #print('Number of cells = ' + str((last_filling_col - first_filling_col + 1) * (last_filling_row - first_filling_row + 1)))
-
-    # Заполнение таблицы
+    filling_range = range_to_a1(first_filling_row,first_filling_col,last_filling_row,last_filling_col)
+    #print('Copiing to range {}{}:{}{}'.format(number_to_fucking_a1(first_filling_col),first_filling_row,number_to_fucking_a1(last_filling_col - 1),last_filling_row - 1))
+    
+    #Заполнение таблицы
     table.update_cells((first_filling_row,first_filling_col),(last_filling_row,last_filling_col),list(flat_copied_range),sheet_PerTrader)
+    print('PerTrader sheet of {} successfuly filled'.format(report.burse))
+
+    #Форматирование заполненного диапазона
+    col_counter = first_filling_col
+    while col_counter <= last_filling_col:
+        fmt = get_effective_format(sheet_PerTrader,number_to_fucking_a1(col_counter) + str(first_filling_row-1))
+        format_cell_range(sheet_PerTrader, '{}{}:{}{}'.format(number_to_fucking_a1(col_counter),str(first_filling_row),number_to_fucking_a1(col_counter),str(last_filling_row)),fmt)
+        col_counter += 1
 #Заполнение листа балансов
 def fill_balances(table,report):
 
     #Определение диапазона копируемых ячеек
-    print('Works on Balances sheet')
     report_sheet = report.workbook.get_sheet_by_name('Balances')
     first_report_row = 2
     first_report_col = 1
     last_report_row = report_sheet.max_row
     last_report_col = first_report_col + 5
-    copied_range_str = range_to_a1(first_report_row, first_report_col, last_report_row, last_report_col)
     copied_range = report_sheet[number_to_fucking_a1(first_report_col) + str(first_report_row):number_to_fucking_a1(last_report_col) + str(last_report_row)]
     flat_copied_range = []
     for tup in copied_range:
@@ -106,26 +129,34 @@ def fill_balances(table,report):
     first_filling_row = len(session_column) + 1
     last_filling_col = first_filling_col + last_report_col - first_report_col
     last_filling_row = first_filling_row + last_report_row - first_report_row
-    filling_range = range_to_a1(first_filling_row, first_filling_col, last_filling_row, last_filling_col)
-    print('Copying {} from report_sheet to {} in PerTrader_sheet.'.format(copied_range_str,filling_range))
-
+    
     # Заполнение таблицы
     table.update_cells((first_filling_row,first_filling_col),(last_filling_row,last_filling_col),list(flat_copied_range),sheet_Balances)
-
-    print('Balances filled')
+    col_counter = first_filling_col
+    while col_counter <= last_filling_col:
+        fmt = get_effective_format(sheet_Balances,number_to_fucking_a1(col_counter) + str(first_filling_row - 1))
+        if fmt.numberFormat:
+            fmt.numberFormat.type = None
+        #print('Formatting {}{}:{}{}'.format(number_to_fucking_a1(col_counter),str(first_filling_row),number_to_fucking_a1(col_counter),str(last_filling_row)))
+        format_cell_range(sheet_Balances, '{}{}:{}{}'.format(number_to_fucking_a1(col_counter), str(first_filling_row), number_to_fucking_a1(col_counter), str(last_filling_row)), fmt)
+        col_counter += 1
+    print('Balances sheet of {} successfuly filled'.format(report.burse))
 #Вспомогательная функция разбиения имени отчёта, для получения инфы по нему
 def parse_report_name(report_file):
-    current_year = report_file[:4]
-    current_month = report_file[4:6]
-    current_day = report_file[6:8]
-    burse = ''
-    counter = 9
-    while report_file[counter] != '_':
-        burse += report_file[counter]
-        counter += 1
-    session = report_file[counter + 1:counter + 2]
-    print('Year ' + current_year + ' Month ' + current_month + ' day ' + current_day + ' burse ' + burse + ' session ' + session)
-    print('Report name successfully parsed')
+    try:
+        current_year = report_file[:4]
+        current_month = report_file[4:6]
+        current_day = report_file[6:8]
+        burse = ''
+        counter = 9
+        while report_file[counter] != '_':
+            burse += report_file[counter]
+            counter += 1
+        session = report_file[counter + 1:counter + 2]
+    except requests.exceptions.RequestException:
+        print("Can't parse filename")
+        SystemExit()
+    print('Open report: ' + 'Year ' + current_year + ' Month ' + current_month + ' day ' + current_day + ' burse ' + burse + ' session ' + session)
     return current_year, current_month, current_day, burse, session
 #Класс отчёта
 class Report():
@@ -204,5 +235,14 @@ def range_to_a1(first_row, first_col, last_row, last_col):
     #end = number_to_fucking_a1(last_col) + str(last_row)
     range = number_to_fucking_a1(first_col) + str(first_row) + ':' + number_to_fucking_a1(last_col) + str(last_row)
     return range
+def check(table, report):
+    summary_sheet = table.find_sheet('Summary_' + str(get_month_from_numbers(report.month)[0:3]))
+    check_value = summary_sheet.cell(summary_sheet.find('Check').row,summary_sheet.find('Check').col + 1).value
+    double_check_value = summary_sheet.cell(summary_sheet.find('Double Check').row,summary_sheet.find('Double Check').col + 1).value
+    print('Check is {} and double check is {}'.format(check_value, double_check_value))
+    if abs(float(check_value.replace(',','.'))) >= 10:
+        print('WARNING!!! Check is ' + check_value)
+    if abs(float(double_check_value.replace(',','.'))) >= 10:
+        print('WARNING!!! Double Check is ' + double_check_value)
 if __name__ == '__main__':
     main()
